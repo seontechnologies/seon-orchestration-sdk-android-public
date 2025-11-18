ab748acc9c511e9db72e96e9ee59d9a528012168
Abbas <abbas.sabetinezhad@seon.io>
2025-11-18 07:46:53 +0100

    Added: Sample App

---
+++ Example/sample/src/main/java/io/seon/id_verification/ui/theme/Theme.kt
@@ -0,0 +1,96 @@
+package io.seon.id_verification.ui.theme
+
+import android.app.Activity
+import android.content.Context
+import android.os.Build
+import android.view.View
+import androidx.activity.ComponentActivity
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+
+private val DarkColorScheme = darkColorScheme(
+    primary = Purple80,
+    secondary = PurpleGrey80,
+    tertiary = Pink80
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = Purple40,
+    secondary = PurpleGrey40,
+    tertiary = Pink40
+
+    /* Other default colors to override
+    background = Color(0xFFFFFBFE),
+    surface = Color(0xFFFFFBFE),
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onTertiary = Color.White,
+    onBackground = Color(0xFF1C1B1F),
+    onSurface = Color(0xFF1C1B1F),
+    */
+)
+
+@Composable
+fun SeonidverificationTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val context = LocalContext.current
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            setWindowColors(context, view, color = colorScheme.primary, isDarkTheme = darkTheme)
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+        }
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
+
+internal fun setWindowColors(
+    context: Context,
+    view: View,
+    color: Color,
+    isDarkTheme: Boolean,
+) {
+    val window = (context as? ComponentActivity ?: return).window
+    val insetsController = WindowCompat.getInsetsController(window, view)
+
+    WindowCompat.setDecorFitsSystemWindows(window, false)
+
+    insetsController.isAppearanceLightStatusBars = true // or false for dark icons
+
+    window.decorView.setOnApplyWindowInsetsListener { decorView, insets ->
+        decorView.setBackgroundColor(color.toArgb())
+        insets
+    }
+
+    insetsController.isAppearanceLightStatusBars = !isDarkTheme
+    insetsController.isAppearanceLightNavigationBars = !isDarkTheme
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # SEON Orchestration Android SDK
+
+# Changelog
+
+## 1.6.3
+-   Initial Version
